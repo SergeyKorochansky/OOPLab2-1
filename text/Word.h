a1f63ebbb85c02f63198441ce042f42d6c2f1eec
Vasyl <Norfolk_20008@email.ua>
2014-10-01 22:57:16 +0300

    Add Word constructor from string

--- text/Word.h
+++ text/Word.h
@@ -8,6 +8,11 @@ public:
 	Word() : std::vector<Symbol>() {}
 	Word(size_type n) :  std::vector<Symbol>(n) {}
 	Word(const Word &word) :  std::vector<Symbol>(word) {}
+	Word(const std::string &word)
+	{
+		for(auto it = word.cbegin(); it != word.end(); ++it)
+			push_back(Symbol(*it));
+	}
 
 	friend std::istream &operator>>(std::istream &stream, Word &word);
 	friend std::ostream &operator<<(std::ostream &stream, Word &word);

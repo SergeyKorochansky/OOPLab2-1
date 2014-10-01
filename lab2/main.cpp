57095e5e525bd69cfe85891db6a28ca73fc05fa3
Vasyl <Norfolk_20008@email.ua>
2014-10-01 23:29:59 +0300

    Remove unneccessary code in main.cpp

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -2,19 +2,6 @@
 
 int main()
 {
-	/*Symbol a('a');
-	Symbol b('b');
-	Symbol one = Symbol::fromDigit(1);
-	
-	Word myWord;
-	myWord.push_back(a);
-	myWord.push_back(b);
-	myWord.push_back(one);
-
-	std::cin >> myWord;
-
-	std::cout << myWord;
-	*/
 	Paragraph myParagraph;
 	std::cin >> myParagraph;
 	std::cout << myParagraph;
